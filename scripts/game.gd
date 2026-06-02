2f10ab22ddad01a7850f748e758e8a3c6fed51bc
Luke Daenzer <70449368+The-Luzer@users.noreply.github.com>
2026-06-01 23:05:22 -0500

    Overwritten base scan mechanic

---
+++ scripts/game.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept") and $ScanCover.visible == true:
+		$ScanCover.hide()
+	elif Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_left") or Input.is_action_just_pressed("ui_right"):
+		$ScanCover.show()
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,8 @@
-# Code originally developed by Mostly Mad Productions
-# https://www.youtube.com/@MostlyMadProductions
-# Feburary 2025
-#
-# Modified by Tyler Jacobson
-# June 2026
-
 extends CharacterBody2D
 
-
-const tile_size: Vector2 = Vector2(32, 32)
+const tile_size: Vector2 = Vector2(16,16)
 var sprite_node_pos_tween: Tween
 
-# Moves player up, down, left, and right based on user input (arrow keys)
-# TODO: Add press and hold functionality
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up") and !$up.is_colliding():
 		_move(Vector2(0, -1))
@@ -22,8 +12,7 @@ func _physics_process(_delta: float) -> void:
 		_move(Vector2(-1, 0))
 	elif Input.is_action_just_pressed("ui_right") and !$right.is_colliding():
 		_move(Vector2(1, 0))
-
-# Moves the player character with delay animation for character sprite
+		
 func _move(dir: Vector2):
 	global_position += dir * tile_size
 	$Sprite2D.global_position -= dir * tile_size
@@ -32,7 +21,4 @@ func _move(dir: Vector2):
 		sprite_node_pos_tween.kill()
 	sprite_node_pos_tween = create_tween()
 	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	sprite_node_pos_tween.tween_property(
-		$Sprite2D, 
-		"global_position", 
-		global_position, 0.185).set_trans(Tween.TRANS_SINE)
+	sprite_node_pos_tween.tween_property($Sprite2D, "global_position", global_position, 0.185)
