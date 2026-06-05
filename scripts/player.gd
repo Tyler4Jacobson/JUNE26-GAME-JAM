244089b7c784b3ea4cf70b350fd46009d0da36bb
John Stromberg <74280994+JohnStromberg@users.noreply.github.com>
2026-06-04 22:23:15 -0500

    fix diagonal movement speed

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,30 +5,43 @@ var sprite_node_pos_tween: Tween
 var timer = 0.0
 
 func _physics_process(delta: float) -> void:
-	var interval = 0.1
+	var base_interval = 0.1
+	var diag_interval = base_interval * sqrt(2)
+	var interval = base_interval
+	var movement_vector = Vector2.ZERO
 	
 	timer += delta
 	#print(timer, interval)
 	# Governer slows down movement
-	if timer >= interval:
+	if timer >= base_interval:
 		if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left") and !$nw.is_colliding():
-			_move(Vector2(-1, -1))	# move northwest
+			movement_vector = Vector2(-1, -1) # move northwest
+			interval = diag_interval
 		elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right") and !$ne.is_colliding():
-			_move(Vector2(1, -1))	# move northeast
+			movement_vector = Vector2(1, -1) # move northeast
+			interval = diag_interval
 		elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left") and !$sw.is_colliding():
-			_move(Vector2(-1, 1))	# move southwest
+			movement_vector = Vector2(-1, 1) # move southwest
+			interval = diag_interval
 		elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right") and !$se.is_colliding():
-			_move(Vector2(1, 1))	# move southeast
+			movement_vector = Vector2(1, 1) # move southeast
+			interval = diag_interval
 		elif Input.is_action_pressed("ui_up") and !$n.is_colliding():
-			_move(Vector2(0, -1))
+			movement_vector = Vector2(0, -1)
 		elif Input.is_action_pressed("ui_down") and !$s.is_colliding():
-			_move(Vector2(0, 1))
+			movement_vector = Vector2(0, 1)
 		elif Input.is_action_pressed("ui_left") and !$w.is_colliding():
-			_move(Vector2(-1, 0))
+			movement_vector = Vector2(-1, 0)
 		elif Input.is_action_pressed("ui_right") and !$e.is_colliding():
-			_move(Vector2(1, 0))
-		timer -= interval
-		
+			movement_vector = Vector2(1, 0)
+				
+		if movement_vector != Vector2.ZERO:
+			if timer >= interval:
+				timer -= interval
+				_move(movement_vector)
+		else:
+			timer -= base_interval
+			
 func _move(dir: Vector2):
 	global_position += dir * tile_size
 	$Sprite2D.global_position -= dir * tile_size
